0c83d7d81c29a2d72676652458d852e3005da6c7
jadiazg02 <joseadiazg02@correo.ugr.es>
2017-07-18 19:57:59 +0200

    Añadido spider bien

---
+++ scrapy_twitter/spiders/streamfilter.py
@@ -0,0 +1,20 @@
+import scrapy
+
+from scrapy_twitter import TwitterStreamFilterRequest, to_item
+
+class StreamFilterSpider(scrapy.Spider):
+    name = "stream-filter"
+    allowed_domains = ["twitter.com"]
+
+    def __init__(self, track = None, *args, **kwargs):
+        if not track:
+            raise scrapy.exceptions.CloseSpider('Argument track not set.')
+        super(StreamFilterSpider, self).__init__(*args, **kwargs)
+        self.track = track.split(',')
+
+    def start_requests(self):
+        return [ TwitterStreamFilterRequest(track = self.track) ]
+
+    def parse(self, response):
+        for tweet in response.tweets:
+            yield to_item(tweet)
