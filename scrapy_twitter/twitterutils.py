dd8a665221e04f7e3218e816771be380a81e1fd7
jadiazg02 <joseadiazg02@correo.ugr.es>
2017-07-19 11:12:53 +0200

    Nuevo deploy.

--- scrapy_twitter/twitterutils.py
+++ scrapy_twitter/twitterutils.py
@@ -8,22 +8,22 @@ import twitter
 
 class TwitterUserTimelineRequest(Request):
 
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         self.screen_name = kwargs.pop('screen_name', None)
         self.count = kwargs.pop('count', None)
         self.max_id = kwargs.pop('max_id', None)
-        super(TwitterUserTimelineRequest, self).__init__(url='http://twitter.com',
-                                                         dont_filter=True,
-                                                         **kwargs)
+        super(TwitterUserTimelineRequest, self).__init__('http://twitter.com',
+                                              *args,
+                                              **kwargs)
 
 
 class TwitterStreamFilterRequest(Request):
 
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         self.track = kwargs.pop('track', None)
-        super(TwitterStreamFilterRequest, self).__init__(url='http://twitter.com',
-                                                         dont_filter=True,
-                                                         **kwargs)
+        super(TwitterStreamFilterRequest, self).__init__('http://twitter.com',
+                                              *args,
+                                              **kwargs)
 
 
 class TwitterResponse(Response):
